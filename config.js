cb799fb7cb3ecfc6671af3c3e8c52c8462745a86
ZTRCOMPANY1 <ztrcompany1@gmail.com>
2026-05-02 15:33:27 -0300

    kaskasasa

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Troque pela URL do seu backend no Render.
 // Exemplo: https://ztr-report-api.onrender.com
-window.ZTR_API_URL = 'https://SEU-SERVER-RENDER.onrender.com';
+window.ZTR_API_URL = 'https://backend-do-site-de-reporte-de-bugs-da.onrender.com';
